180c511bc0688ef229e27bece06223f76b9e32ec
5ach.in <www.5ach.in@outlook.com>
2021-03-26 00:21:28 +0000

    custom

--- ad/custom17/script.js
+++ ad/custom17/script.js
@@ -2,9 +2,9 @@ function pauseAd(){}
 function resumeAd(){}
 
 var latestAppV = '4.3.0'
-var randomIntegerVal = 1//randomInteger(1, 10);
+var randomIntegerVal = randomInteger(1, 10);
 if(userCurrentAppVersion==latestAppV && randomIntegerVal % 2 === 0){
-    window.location.href = "https://sachin-s-shah.github.io/ad/custom14/index.html";
+    window.location.href = "https://sachin-s-shah.github.io/ad/customNaz2/index.html";
 }
 else{
     if (document.readyState === "complete") {
